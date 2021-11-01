e11cc471e9542a84063b5a7546bf399d362cd18b
Frumentius Daneswara <frumentius44@gmail.com>
2021-11-01 13:41:42 +0700

    update function

--- chat.js
+++ chat.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
                               <span class="px-3 pt-2 pb-4 chat-bubble float-end text-white position-relative" style="background-color: #38435F; min-width: 10%; max-width: 70%; border-radius: .5rem!important;">
                                     ${message}
                                     <span class="text-white-50 position-absolute bottom-0 end-0 me-2 mb-1" style="font-size: .8rem;">
-                                          ${moment().format('LT')}
+                                          ${moment().format('HH:mm')}
                                     </span>
                               </span>
                         </div>`
